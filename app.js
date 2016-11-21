5503fa7548b007c9f80af7a939cc5ff892376941
Xenoxo <timwyc@gmail.com>
2016-11-21 15:23:12 +0100

    Wire up emit and reach end of tutorial

--- app.js
+++ app.js
@@ -41,12 +41,8 @@ app.use(function(req, res, next) {
 // })
 
 io.on('connection', function(socket){
-  // console.log('a user connected');
-  // socket.on('disconnect', function(){
-  //   console.log('user disconnected');
-  // });
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    io.emit('chat message', msg)
   });
 });
 
